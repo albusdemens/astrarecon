7881c10255344a5a7f8958aced6322e3ee6181bc
albusdemens <mannaro85@gmail.com>
2017-06-12 18:06:45 +0200

    Updated version

--- getdata.py
+++ getdata.py
@@ -7,12 +7,14 @@ import numpy as np
 # Load the npy file from getdata.py (on panda2)
 A = np.load('/home/gpu/astra_data/April_2017_sundaynight/dataarray.npy')
 
-A_3d = np.zeros([A.shape[2], A.shape[3],  A.shape[4]])
+A_3d = np.zeros([A.shape[3], A.shape[2],  A.shape[4]])	# (x, omega, y)
 # Sum images recorded at the same omega
 for oo in range(A.shape[2]):
 	A_oo = np.zeros([A.shape[3], A.shape[4]])
 	A_oo[:,:] = A[:, :, oo, :, :].sum()
-	A_3d[oo, :, :] = A_oo[:,:]
+	for ii in range(A.shape[3]):
+		for jj in range(A.shape[4]):
+			A_3d[ii, oo, jj] = A_oo[ii,jj]
 
 # Save the result
 np.save('../astra_data/April_2017_sundaynight/A_3d.npy', A_3d)
