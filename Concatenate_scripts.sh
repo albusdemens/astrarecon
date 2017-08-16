ee57c8f1330f1362a83b40bd9f6f432864a99853
albusdemens <mannaro85@gmail.com>
2017-08-17 00:11:39 +0200

    Start frash

--- Concatenate_scripts.sh
+++
@@ -1,9 +0,0 @@
-cd /home/nexmap/alcer/DFXRM/Recon3D/ &&
-python getdata.py /u/data/andcj/hxrm/Al_april_2017/topotomo/sundaynight topotomo_frelon_far_ /home/nexmap/alcer/DFXRM/bg_refined topotomo_frelon_far_ 256,256 300,300 /u/data/alcer/DFXRM_rec Rec_test 0.785 -3.319 &&
-ls /u/data/andcj/hxrm/Al_april_2017/topotomo/sundaynight/ > /u/data/alcer/DFXRM_rec/Rec_test/List_images.txt &&
-python image_properties.py &&
-cd /home/nexmap/alcer/Astra/ &&
-python Clean_sum_img.py &&
-matlab -nodesktop -nodisplay -r "Fabio_isolated_blob.m" &&
-scp /u/data/alcer/DFXRM_rec/Rec_test/Astra_input.npy gpu@130.225.86.15:/home/gpu/astra_data/April_2017_sundaynight/ &&
-python recon.py
--- README.md
+++
@@ -1,3 +0,0 @@
-# Astrarecon
-
-Does 3D reconstruction of topo-tomography data from ID06 using the Astra toolbox.
